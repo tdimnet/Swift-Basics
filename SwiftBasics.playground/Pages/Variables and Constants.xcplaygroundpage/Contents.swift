066d3d1f5b478f3d3b064a9951c2ac223fdeb0e8
tdimnet <thomas.dimnet@gmail.com>
2018-03-30 19:50:00 +0200

    Create a new PlayGround page and save the previous one as another page.

---
+++ SwiftBasics.playground/Pages/Variables and Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+// Variables
+
+var str = "Hello, playground"
+str = "Hello, world!"
+
+var number = 10
+number = 20
+
+// Constants
+let language = "Swift"
+
+// Naming Conventions
+
+/*
+ Rule #1: Spaces not allowed
+ let programming language = "Objective-C"
+*/
+
+/*
+ Rule #2: Use camel case
+ let programminglanguage = "Objective-C" // Wrong!
+ */
+
+
+
+let programmingLanguage = "Objective-C"
