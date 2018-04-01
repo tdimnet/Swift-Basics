3d6af0974117cb3b185d9862f583ce75a04d334f
tdimnet <thomas.dimnet@gmail.com>
2018-04-01 23:42:41 +0200

    Add equal to and greater than operator.

--- SwiftBasics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -7,3 +7,24 @@ let area: Double = height * width // Area in square feet
 
 // 1 square meter = 1 square foot / 10.764
 let areaInMeters: Double = area / 10.764
+
+// Equals Operator (Note: different from Asignment Operator)
+
+let string1: String = "Hello!"
+let string2: String = "Hello!"
+let string3: String = "hello"
+
+string1 == string2
+string1 == string3
+
+// Not equal
+
+string1 != string2
+string1 != string3
+
+// Greater than
+
+1 > 2
+2 > 1
+
+"a" > "b"
