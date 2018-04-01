db600718e34af44c61f21e66dc79719a1f176732
tdimnet <thomas.dimnet@gmail.com>
2018-04-01 23:34:13 +0200

    Start working with operator: binary operators.

---
+++ SwiftBasics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+// Binary Operators
+
+let height: Double = 12 // in Feet
+let width: Double = 10 // in Feet
+
+let area: Double = height * width // Area in square feet
+
+// 1 square meter = 1 square foot / 10.764
+let areaInMeters: Double = area / 10.764
